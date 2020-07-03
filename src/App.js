ef2678883997408d04a96c36bc03ddbbccec36b8
Ibrahim <mohammadibrahim.ibrahim9@gmail.com>
2020-07-03 19:48:43 +0500

    Adding material-ui

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import Landing from './components/landing';
 import Teamcard from './components/team/teamcard';
 import 'tachyons';
 import Team from './components/team/team';
+import {
+  CssBaseline
+} from '@material-ui/core';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4abdac'
+    },
+    secondary: {
+      main: '#f7b733'
+    },
+    info: {
+      main: '#dfdce3'
+    },
+    success: {
+      main: '#fc4a1a'
+    }
+  }
+})
+
+
+
 function App() {
   return (
-    <div >
-       <Landing/>
-      {/* <Team/> */}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Landing />
 
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,21 +2,17 @@ import React from 'react'
 import '../css/navbar.css';
 import '../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 class Navbar extends React.Component {
-    render() {
-        return (
-            <div id="main-nav">
-                <div id="sub-nav">
+	render() {
+		return (
+			<div id="main-nav">
+				<div id="sub-nav">
+					<li id="sub-nav-li"><i class="fas fa-phone-alt"></i><a>Contact us</a></li>
+					<li id="sub-nav-li"><a>About us</a></li>
+				</div>
 
-                    <li id="sub-nav-li"><i class="fas fa-phone-alt"></i><a>Contact us</a></li>
-                    <li id="sub-nav-li"><a>About us</a></li>
-
-
-
-                </div>
-
-            </div>
-        )
-    }
+			</div>
+		)
+	}
 }
 
 export default Navbar
