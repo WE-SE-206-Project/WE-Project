0abac84137bd7b9ee2f91e7c39019a3c9d78b80c
Ibrahim <rmibrahim00@gmail.com>
2020-08-22 21:24:18 +0500

    few changes

--- src/Comps/Navbar.js
+++ src/Comps/Navbar.js
@@ -87,7 +87,7 @@ export default function Navbar() {
               !auth.status
                 ?
                 <>
-                  <Button className={classes.btn}>About Us</Button>
+                  <Button className={classes.btn} onClick={() => history.push('/contact_us')}>About Us</Button>
                   <Button className={classes.btn} onClick={() => history.push('/contact_us')}>Contact Us</Button>
                 </>
                 :
@@ -112,7 +112,10 @@ export default function Navbar() {
                     !auth.status
                       ?
                       <>
-                        <MenuItem onClick={handleClose}>About Us</MenuItem>
+                        <MenuItem onClick={() => {
+                          handleClose();
+                          history.push('/about_us');
+                        }}>About Us</MenuItem>
                         <MenuItem onClick={() => {
                           handleClose();
                           history.push('/contact_us');
