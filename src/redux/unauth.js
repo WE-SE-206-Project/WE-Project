ea03dafc275cf0651d309f3e88f12c2259bfbaa4
Ibrahim <rmibrahim00@gmail.com>
2020-08-22 19:28:49 +0500

    Adding minor details

--- src/redux/unauth.js
+++ src/redux/unauth.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const slice = createSlice({
   name: 'unauth',
   initialState: {
-    role: 'org'
+    role: 'user'
   },
   reducers: {
     setRole: (state, { payload }) => {
